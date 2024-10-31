9a06f16b18da8688718b1f65149ea161d40740d8
Loading-Yann <yann.jadeau@gmail.com>
2024-10-31 09:50:22 +0100

    animation de la flèche de apropos( début manque début rotation)

--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import './_Apropos.scss';
 import { aboutList } from '../data/aboutList';
+import { ICONS } from '../utils/config';
 
 function Apropos() {
   const [openIndexes, setOpenIndexes] = useState([]);
@@ -27,12 +28,8 @@ function Apropos() {
           <div key={index} className="apropos-item">
             <button className="apropos-button" onClick={() => handleToggle(index)}>
               {item.title}
-              <span className="arrow">
-                {openIndexes.includes(index) ? (
-                  <i className="fas fa-caret-up"></i> // Flèche vers le haut
-                ) : (
-                  <i className="fas fa-caret-down"></i> // Flèche vers le bas
-                )}
+              <span className={`arrow ${openIndexes.includes(index) ? 'open' : ''}`}>
+                <i className={`fa-solid ${openIndexes.includes(index) ? ICONS.arrowUp : ICONS.arrowDown}`}></i>
               </span>
             </button>
 
---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+
+export const ICONS = {
+  arrowUp: 'fa-chevron-up',
+  arrowDown: 'fa-chevron-down'
+};
